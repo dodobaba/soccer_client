3e32c93412d62c9d536feff2ac39ef4b3d2322b0
Ekain Cao <>
2020-03-18 15:14:08 +0800

    all teams infomation was ok

--- lib/models/teams_detail.dart
+++ lib/models/teams_detail.dart
@@ -10,8 +10,22 @@ class TeamsDetail extends StatelessWidget {
   Widget build(BuildContext context) {
     print(this.data);
     return Card(
-      margin: EdgeInsets.fromLTRB(20, (100 - scale * 50), 20, 5),
-      child: Column(children: [Text(this.data['nameChs'])]),
-    );
+        margin: EdgeInsets.fromLTRB(20, (100 - scale * 50), 20, 5),
+        child: Container(
+          padding: EdgeInsets.all(20),
+          child: Column(
+            children: [
+            CircleAvatar(
+              backgroundImage: NetworkImage(this.data['logo']),
+            ),
+            Text(this.data['nameChs']),
+            Text(this.data['nameCht']),
+            Text(this.data['nameEn']),
+            Text('成立时间：${this.data['foundingDate']}'),
+            Text('主场：${this.data['areaCn']}/${data['gymCn']}  ${this.data['areaEn']}/${data['gymEn']} 坐席：${data['apacity']}'),
+            Text('主教练：${this.data['coachCn']} / ${data['coachEn']}'),
+            Text('地址：${this.data['addrCn']}/${data['addrEn']}'),
+          ]),
+        ));
   }
 }
